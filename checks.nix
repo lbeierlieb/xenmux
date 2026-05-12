9ccb634be08a881a77ddb1b3cb9166572fbd7c25
Lukas Beierlieb <lukas.beierlieb@cyberus-technology.de>
2026-05-12 13:49:01 +0200

    xenmux, checks: move test-specific code into checks

--- checks.nix
+++ checks.nix
@@ -6,6 +6,28 @@
       { pkgs, ... }:
       {
         imports = [ (import ./xenmux.nix) ];
+
+        boot.loader.systemd-boot.enable = true;
+        virtualisation.useBootLoader = true;
+        virtualisation.installBootLoader = true;
+        virtualisation.useEFIBoot = true;
+        environment.systemPackages = with pkgs; [
+          pciutils
+        ];
+        systemd.services.renamehvc = {
+          wantedBy = [ "backdoor.service" ];
+          requires = [
+            "dev-hvc0.device"
+            "dev-hvc1.device"
+          ];
+          before = [
+            "backdoor.service"
+          ];
+          serviceConfig.Type = "oneshot";
+          script = ''
+            mv /dev/hvc1 /dev/hvc0
+          '';
+        };
       };
     testScript = ''
       machine.wait_for_unit("multi-user.target", timeout=300)
--- xenmux.nix
+++ xenmux.nix
@@ -1,25 +1,4 @@
 { pkgs, ... }:
 {
-  boot.loader.systemd-boot.enable = true;
   virtualisation.xen.enable = true;
-  virtualisation.useBootLoader = true;
-  virtualisation.installBootLoader = true;
-  virtualisation.useEFIBoot = true;
-  environment.systemPackages = with pkgs; [
-    pciutils
-  ];
-  systemd.services.renamehvc = {
-    wantedBy = [ "backdoor.service" ];
-    requires = [
-      "dev-hvc0.device"
-      "dev-hvc1.device"
-    ];
-    before = [
-      "backdoor.service"
-    ];
-    serviceConfig.Type = "oneshot";
-    script = ''
-      mv /dev/hvc1 /dev/hvc0
-    '';
-  };
 }
