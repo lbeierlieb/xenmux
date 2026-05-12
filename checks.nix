1dcc318a143bcc379daf5a3152f10bbd31d09ac6
Lukas Beierlieb <lukas.beierlieb@cyberus-technology.de>
2026-05-12 14:24:26 +0200

    checks, xenmux: parameterize by Xen version

--- checks.nix
+++ checks.nix
@@ -1,36 +1,59 @@
 { pkgs }:
-{
-  xen4_20_2 = pkgs.testers.runNixOSTest {
-    name = "xen4_20_2 boot";
-    nodes.machine =
-      { pkgs, ... }:
-      {
-        imports = [ (import ./xenmux.nix) ];
+let
+  xen_version_test =
+    {
+      major,
+      minor,
+      extra,
+    }:
+    let
+      maj = toString major;
+      min = toString minor;
+      ex = toString extra;
+    in
+    pkgs.testers.runNixOSTest {
+      name = "xen${maj}_${min}_${ex} boot";
+      nodes.machine =
+        { pkgs, ... }:
+        {
+          imports = [ (import ./xenmux.nix) ];
 
-        boot.loader.systemd-boot.enable = true;
-        virtualisation.useBootLoader = true;
-        virtualisation.installBootLoader = true;
-        virtualisation.useEFIBoot = true;
-        systemd.services.renamehvc = {
-          wantedBy = [ "backdoor.service" ];
-          requires = [
-            "dev-hvc0.device"
-            "dev-hvc1.device"
-          ];
-          before = [
-            "backdoor.service"
-          ];
-          serviceConfig.Type = "oneshot";
-          script = ''
-            mv /dev/hvc1 /dev/hvc0
-          '';
+          xenmux = {
+            enable = true;
+            inherit major minor extra;
+          };
+
+          boot.loader.systemd-boot.enable = true;
+          virtualisation.useBootLoader = true;
+          virtualisation.installBootLoader = true;
+          virtualisation.useEFIBoot = true;
+          systemd.services.renamehvc = {
+            wantedBy = [ "backdoor.service" ];
+            requires = [
+              "dev-hvc0.device"
+              "dev-hvc1.device"
+            ];
+            before = [
+              "backdoor.service"
+            ];
+            serviceConfig.Type = "oneshot";
+            script = ''
+              mv /dev/hvc1 /dev/hvc0
+            '';
+          };
         };
-      };
-    testScript = ''
-      machine.wait_for_unit("multi-user.target", timeout=300)
-      machine.succeed("cat /sys/hypervisor/version/major | grep 4")
-      machine.succeed("cat /sys/hypervisor/version/minor | grep 20")
-      machine.succeed("cat /sys/hypervisor/version/extra | grep 2")
-    '';
+      testScript = ''
+        machine.wait_for_unit("multi-user.target", timeout=300)
+        machine.succeed("cat /sys/hypervisor/version/major | grep ${maj}")
+        machine.succeed("cat /sys/hypervisor/version/minor | grep ${min}")
+        machine.succeed("cat /sys/hypervisor/version/extra | grep ${ex}")
+      '';
+    };
+in
+{
+  xen4_20_2 = xen_version_test {
+    major = 4;
+    minor = 20;
+    extra = 2;
   };
 }
--- xenmux.nix
+++ xenmux.nix
@@ -1,4 +1,20 @@
-{ pkgs, ... }:
 {
-  virtualisation.xen.enable = true;
+  pkgs,
+  config,
+  lib,
+  ...
+}:
+let
+  cfg = config.xenmux;
+in
+{
+  options.xenmux = {
+    enable = lib.mkEnableOption "xenmux";
+    major = lib.mkOption { };
+    minor = lib.mkOption { };
+    extra = lib.mkOption { };
+  };
+  config = lib.mkIf cfg.enable {
+    virtualisation.xen.enable = true;
+  };
 }
