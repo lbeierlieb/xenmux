0745cfae1b297cca48044588ecab13a2485b4f6d
Lukas Beierlieb <lukas.beierlieb@cyberus-technology.de>
2026-05-12 14:50:57 +0200

    xenmux: build xen package for specified version

--- xenmux.nix
+++ xenmux.nix
@@ -6,6 +6,27 @@
 }:
 let
   cfg = config.xenmux;
+  xen_package =
+    {
+      major,
+      minor,
+      extra,
+    }:
+    let
+      maj = toString major;
+      min = toString minor;
+      ex = toString extra;
+      version_string = "${maj}.${min}.${ex}";
+    in
+    pkgs.xen.overrideAttrs (old: {
+      version = version_string;
+      src = pkgs.fetchFromGitHub {
+        owner = "xen-project";
+        repo = "xen";
+        tag = "RELEASE-${version_string}";
+        hash = "sha256-mqVuMqvSNIEGynnVHvg8M/4DG7sDls3tf32EQsn0PsI=";
+      };
+    });
 in
 {
   options.xenmux = {
@@ -16,5 +37,10 @@ in
   };
   config = lib.mkIf cfg.enable {
     virtualisation.xen.enable = true;
+    virtualisation.xen.package = xen_package {
+      major = cfg.major;
+      minor = cfg.minor;
+      extra = cfg.extra;
+    };
   };
 }
